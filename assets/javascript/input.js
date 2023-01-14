383d50aa0f3c0e4ce07d1ee2f2e64e1043aca625
jonthorell <jthorell@gmail.com>
2023-01-14 14:47:17 +0000

    Javascript added to textarea in contact form

---
+++ assets/javascript/input.js
@@ -0,0 +1,8 @@
+// Array.from( document.querySelectorAll('[data-expand]'), (input)=>{
+//   let parent = input.parentNode;
+//   function updateSize(){
+//     parent.dataset.value = input.value
+//   }
+//   input.addEventListener('input', updateSize);
+//   updateSize();
+// });

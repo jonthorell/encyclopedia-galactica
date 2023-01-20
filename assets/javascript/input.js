a45ea81bd309fb7307dc9d75629468ba0c319901
jonthorell <jthorell@gmail.com>
2023-01-20 11:14:09 +0000

    Remove the unused javascript

--- assets/javascript/input.js
+++
@@ -1,8 +0,0 @@
-// Array.from( document.querySelectorAll('[data-expand]'), (input)=>{
-//   let parent = input.parentNode;
-//   function updateSize(){
-//     parent.dataset.value = input.value
-//   }
-//   input.addEventListener('input', updateSize);
-//   updateSize();
-// });
